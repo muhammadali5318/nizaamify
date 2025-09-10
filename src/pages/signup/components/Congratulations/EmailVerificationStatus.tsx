6318f82d7a158850d17ec1d650e849de40382c82
Muhammad Ali <muhammed.ali@31g.co.uk>
2025-09-10 08:49:21 +0000

    Merged in MFP-100_email-verification-remaining-screens (pull request #15)
    chore: Added all verifications email screens
    
    * chore: Added all verifications email screens
    
    * Fix: resolved lint errors

---
+++ src/pages/signup/components/Congratulations/EmailVerificationStatus.tsx
@@ -0,0 +1,48 @@
+import { Box, Button } from '@mui/material'
+import styles from './EmailVerificationStatus.module.scss'
+import { ReactNode, MouseEventHandler } from 'react'
+
+type EmailVerificationStatusProps = {
+  iconSrc?: string
+  iconAlt?: string
+  buttonText?: string
+  onButtonClick?: MouseEventHandler<HTMLButtonElement>
+  children: ReactNode
+  footer?: ReactNode
+}
+
+const EmailVerificationStatus = ({
+  iconSrc = '/assets/warning.svg',
+  iconAlt = 'verification icon',
+  buttonText = 'Send new verification email',
+  onButtonClick,
+  children,
+  footer
+}: EmailVerificationStatusProps) => {
+  return (
+    <Box className={styles.verificationStatusRoot}>
+      <Box>
+        <img className={styles.iconDimension} src={iconSrc} alt={iconAlt} />
+      </Box>
+
+      <Box className={styles.verificationStatusContainer}>{children}</Box>
+
+      <Box>
+        <Button
+          className='width--100'
+          size='large'
+          variant='contained'
+          onClick={onButtonClick}
+        >
+          {buttonText}
+        </Button>
+      </Box>
+
+      {footer && (
+        <Box className={styles.verificationStatusFooter}>{footer}</Box>
+      )}
+    </Box>
+  )
+}
+
+export default EmailVerificationStatus
