5e507f965bc9fa8d7ad1e5ef0132cb24a774632f
M.Ali <muhammed.ali@31g.co.uk>
2026-03-11 15:04:03 +0500

    MFP-997

--- src/layouts/applayout/AppLayout.tsx
+++ src/layouts/applayout/AppLayout.tsx
@@ -30,7 +30,6 @@ import { useUserDetailsInActivePractice } from 'src/hooks/useUserDetailsInActive
 export default function AppLayout() {
   const location = useLocation()
 
-  const isMonaiAgentRoute = location.pathname.startsWith('/monai-agent')
   const permissionsByCategory = useSelector(selectPermissionsByCategory)
   const { isOnboardingCompleted, isActivePracticeSubscribed } =
     useActivePractice()
@@ -329,17 +328,8 @@ export default function AppLayout() {
                           }}
                           sx={{
                             minHeight: 44,
-                            border:
-                              isActive && isMonaiAgentRoute
-                                ? '2px solid transparent'
-                                : 'none',
-                            background:
-                              isActive && isMonaiAgentRoute
-                                ? `
-      linear-gradient(var(--grey-100), var(--grey-100)) padding-box,
-      linear-gradient(90deg, #000000, #C27961, #FFEA00, #00FF04,#00B2FF,#9D00FF,#FF0080) border-box
-    `
-                                : '#F5F5F5',
+                            border: 'none',
+                            background: '#F5F5F5',
                             margin: '0 auto',
                             justifyContent: showLabels ? 'initial' : 'center',
                             width: showLabels ? 'auto' : '56px',
