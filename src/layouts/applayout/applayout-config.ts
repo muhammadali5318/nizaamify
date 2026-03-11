bd8e2bdda5c223ee36273467147fd302b19fe575
M.Ali <muhammed.ali@31g.co.uk>
2026-03-11 15:29:16 +0500

    MFP-1001

--- src/layouts/applayout/applayout-config.ts
+++ src/layouts/applayout/applayout-config.ts
@@ -40,6 +40,8 @@ export const menuSections: { title: string; items: MenuItemData[] }[] = [
         to: paths.expense,
         activeIcon: 'active-wallet.svg',
         inactiveIcon: 'expense-inactive.svg',
+        tooltipContent:
+          'Items such as owners withdrawals, capital loans/injections or tax matters which do not belong in the P&L statement are recorded here',
         moduleId: 'expenses'
       },
       {
